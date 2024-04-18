1544edbfaf3000505587f67d3bb05ed732bd234b
Mukul Agrawal <muagrawa@adobe.com>
2024-04-18 13:41:51 +0530

    A clock showing seconds hand

--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,49 @@
-#include <iostream>
+#include <cmath>                   // for sin, cos
+#include <ftxui/dom/elements.hpp>  // for canvas, Element, separator, hbox, operator|, border
+#include <ftxui/screen/screen.hpp>  // for Pixel
+#include <memory>   // for allocator, shared_ptr, __shared_ptr_access
+#include <string>   // for string, basic_string
+#include <utility>  // for move
+#include <vector>   // for vector, __alloc_traits<>::value_type
+ 
+#include "ftxui/component/component.hpp"  // for Renderer, CatchEvent, Horizontal, Menu, Tab
+#include "ftxui/component/component_base.hpp"      // for ComponentBase
+#include "ftxui/component/event.hpp"               // for Event
+#include "ftxui/component/mouse.hpp"               // for Mouse
+#include "ftxui/component/screen_interactive.hpp"  // for ScreenInteractive
+#include "ftxui/dom/canvas.hpp"                    // for Canvas
+#include "ftxui/screen/color.hpp"  // for Color, Color::Red, Color::Blue, Color::Green, ftxui
+#include "ftxui/component/loop.hpp" 
 
-#include "ftxui/dom/elements.hpp"
-#include "ftxui/screen/screen.hpp"
-#include "ftxui/screen/string.hpp"
-
+#define PI 3.14159265
 int main(void) {
   using namespace ftxui;
 
-  auto summary = [&] {
-    auto content = vbox({
-        hbox({text(L"- done:   "), text(L"3") | bold}) | color(Color::Green),
-        hbox({text(L"- active: "), text(L"2") | bold}) | color(Color::RedLight),
-        hbox({text(L"- queue:  "), text(L"9") | bold}) | color(Color::Red),
-    });
-    return window(text(L" Summary "), content);
-  };
+  //clock second arm state (angle in radian)
+  double radian = (PI / 2.0);
 
-  auto document =  //
-      vbox({
-          hbox({
-              summary(),
-              summary(),
-              summary() | flex,
-          }),
-          summary(),
-          summary(),
-      });
+  //clock component
+  auto clock_component = Renderer([&] {
+    int canvas_w = 101, canvas_h = 101;
+    auto c = Canvas(canvas_w, canvas_h);
 
-  // Limit the size of the document to 80 char.
-  document = document | size(WIDTH, LESS_THAN, 80);
+    // Circle, not filled
+    int center_x = 50, center_y = 40, radius = 40;
+    c.DrawPointCircle(center_x, center_y, radius);
+    c.DrawPointLine(center_x, center_y, center_x + radius*cos(radian), center_y - radius*sin(radian));
 
-  auto screen = Screen::Create(Dimension::Full(), Dimension::Fit(document));
-  Render(screen, document);
-
-  std::cout << screen.ToString() << '\0' << std::endl;
-
+    return canvas(std::move(c)) | border;
+  });
+  
+  //interactive screen and a custom loop
+  auto screen = ScreenInteractive::FitComponent();
+  Loop loop(&screen, clock_component);
+ 
+  while (!loop.HasQuitted()) {
+    loop.RunOnce();
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    //update state
+    radian -= PI/30.0;
+  }
   return EXIT_SUCCESS;
 }
